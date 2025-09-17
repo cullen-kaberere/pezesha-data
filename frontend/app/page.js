5128febd45500e6f15384baa5ae6adedfaa9496d
cullen kaberere <kabesh86@gmail.com>
2025-09-17 11:04:11 +0300

    Update fetch URL in CsvUploader to use production endpoint

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -38,7 +38,7 @@ export default function CsvUploader() {
       const formData = new FormData()
       formData.append("file", selectedFile)
 
-      const response = await fetch("http://localhost:5000/upload", {
+      const response = await fetch("https://pezesha-data.onrender.com/upload", {
         method: "POST",
         body: formData,
       })
